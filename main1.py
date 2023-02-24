64c171f7a8ff02b7c34aafff6ff0f35eb3171b4a
jdeora <123129274+jdeora@users.noreply.github.com>
2023-02-24 18:49:16 -0500

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+def get_repo_commits(user_id):
+    repos_url = f"https://api.github.com/users/{user_id}/repos"
+    response = requests.get(repos_url)
+    repos = json.loads(response.text)
+
+    for repo in repos:
+        repo_name = repo["name"]
+        commits_url = f"https://api.github.com/repos/{user_id}/{repo_name}/commits"
+        response = requests.get(commits_url)
+        commits = json.loads(response.text)
+        num_commits = len(commits)
+        print(f"Repo: {repo_name} Number of commits: {num_commits}")
+get_repo_commits("jdeora")
