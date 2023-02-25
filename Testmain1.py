55933216dbb8bc942ddc6ab659d1f0bf2b2fdbc4
jdeora <123129274+jdeora@users.noreply.github.com>
2023-02-24 21:18:15 -0500

    Update Testmain1.py

--- Testmain1.py
+++ Testmain1.py
@@ -4,6 +4,10 @@ from main1 import get_repo_commits
 class TestGitHubAPI(unittest.TestCase):
     def test_get_repo_commits(self):
         get_repo_commits("jdeora")
+    def test_get_repo_commits(self):
+        get_repo_commits("john145")
+    def test_get_repo_commits(self):
+        get_repo_commits("nouser")
 
 
 if __name__ == '__main__':
