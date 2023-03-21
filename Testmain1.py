e9aaa244b5c3b083fa924d36dd3ad845ec3fce0a
jdeora <123129274+jdeora@users.noreply.github.com>
2023-03-21 19:33:42 -0400

    Update Testmain1.py

--- Testmain1.py
+++ Testmain1.py
@@ -1,19 +1,22 @@
 import unittest
+import pytest
 from main1 import get_repo_commits
 
 class TestGitHubAPI(unittest.TestCase):
     def test_get_repo_commits(self):
         get_repo_commits("jdeora")
-    def test_invalid_user_id():
-    with pytest.raises(Exception):
-        get_repo_commits("invalid_user_id")
-    def test_private_repos():
-        get_repo_commits("user_with_private_repos")
-    # Assert that no output for private repositories
-    def test_empty_repo():
-        get_repo_commits("user_with_empty_repo")
-    # Assert that "Number of commits: 0" for empty repositories
-
+    
+    def test_invalid_user_id(self):
+        with self.assertRaises(Exception):
+            get_repo_commits("invalid_user_id")
+    
+    def test_private_repos(self):
+        with self.assertRaises(Exception):
+            get_repo_commits("user_with_private_repos")
+    
+    def test_empty_repo(self):
+        with self.assertRaises(Exception):
+            get_repo_commits("user_with_empty_repo")
 
 if __name__ == '__main__':
     unittest.main()
